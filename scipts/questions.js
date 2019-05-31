cac1735e960855d6db6206ae19ff341da6610dc9
Bartosz90 <36510019+Bartosz90@users.noreply.github.com>
2019-05-31 18:38:05 +0200

    Update questions.js

--- scipts/questions.js
+++ scipts/questions.js
@@ -1,9 +1,22 @@
 const questions = [
     ["The # symbol specifies that the selector is?", "Id", "tag", "class", "first"],
-    ["Which is not a JavaScript data type?", "Double", "Undefined", "boolean", "string"],
+    ["Which is not a JavaScript data type?", "double", "Undefined", "boolean", "string"],
     ["What property is used to change the text color of an element?", "color", "fontcolor", "textcolor", "backgroundColor"],
     ["what value is given for the left margin: margin: 5px 10px 3px 8px; ?", "8px", "5px", "3px", "10px"],
     ["const a = []; What does 'typeof a' return?", "object", "array", "undefined", "string"],
     ["Which of these is one of the keywords used to create a JavaScript variable?", "let", "variable", "varies", "string"],
-    ["Inside which HTML element do we put the JavaScript?", "script", "javascript", "JS", "link"]
+    ["Inside which HTML element do we put the JavaScript?", "script", "javascript", "JS", "link"],
+    ['How do you call the function "myFunction"?', 'myFunction()', 'func myFunction()', 'None of those', 'myFunction'],
+    ['Which HTML5 element defines navigation links?', 'nav', 'navigate', 'navigation', 'links'],
+    ['What is jQuery?', 'a library', 'a framework', 'jQuery?', 'none of these'],
+    ['Which of these is not a HTML5 element?', 'blink', 'header', 'main', 'section'],
+    ['Method that returns the character at the specified index.', 'charAt()', 'charCodeAt()', 'concat()', 'indexOf'],
+    ['Method that prints the information to the console.', 'console.log(info)', 'console(info)', 'print(info)', 'log(info)'],
+    ['Which doctype is correct for HTML5?', '!DOCTYPE HTML', '!DOCTYPE HTML5', '!DOCTYPE', 'html'],
+    ['Which snippet of CSS is commonly used to center a website horizontally?', 'margin: 0 auto', 'site-align: center', 'margin: center', 'margin: auto 0'],
+    ['Removes, and then returns last element from array.', 'pop()', 'push()', 'map()', 'join()'],
+    ['Array method', 'forEach()', 'floor()', 'ceil()', 'round()'],
+    ['Is NOT an operator.', 'NaN', '--', '+', '%'],
+    ['Is NOT a background-size property value.', 'center', 'contain', 'cover', 'initial'],
+    ['Is NOT a display property value.', 'screen', 'flex', 'inline', 'block']
 ]
